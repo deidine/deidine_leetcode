d787de7a4a9af0d19412e5fea28179c47e1da6c2
deidine/cheigeur <92623952+deidine@users.noreply.github.com>
2024-09-10 09:32:29 +0000

    deidine

---
+++ Medium/add-two-numbers.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+     int[] l1Array=int[];
+     int[] l2Array=int[];
+
+    }
+}
+
+/*
+ * reverse order
+ * 1-rereverse the list
+ * 2-sum i1+12
+ * 3-carry if sums more then 10
+ * L1 val=2,next=4
+ * L1 val=4,next=3
+ * L2 val=5,next=6
+ * L2 val=6,next=4
+ * arr1=[2,4,3]
+ * arr2=[5,6,4]
+ * res=[2+5,4+6,3+4]
+ * reverRes=[3+4,4+6,2+5]
+ * 
+ * 
+ * 
+ * 
+ */
