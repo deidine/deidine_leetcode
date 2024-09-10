df29695543b5652fafc0a8b94bf9b9c20c27468d
deidine/cheigeur <92623952+deidine@users.noreply.github.com>
2024-09-10 09:35:34 +0000

    deidien

--- Medium/add-two-numbers.java
+++ Medium/add-two-numbers.java
@@ -7,11 +7,19 @@
  * ListNode(int val) { this.val = val; }
  * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
- */
+ */ 
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+ ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-     int[] l1Array=int[];
-     int[] l2Array=int[];
+     int[] l1Array;
+     int[] l2Array;
+    
 
     }
 }
